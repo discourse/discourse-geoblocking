ea0cb4344036e0c5ec702a7e064f62762e9eb477
Bianca Nenciu <nbianca@users.noreply.github.com>
2022-03-11 17:17:41 +0200

    DEV: Use DistributedCache to cache site settings (#20)
    Previous implementation worked for only one instance.

--- plugin.rb
+++ plugin.rb
@@ -289,37 +289,39 @@ after_initialize do
   require_relative("app/controllers/geoblocking_controller")
 
   module ::DiscourseGeoblocking
+    PLUGIN_NAME = 'discourse-geoblocking'
+
+    def self.cache
+      @cache ||= DistributedCache.new(PLUGIN_NAME)
+    end
+
+    def self.reset_cache!
+      cache.clear
+    end
+
     def self.allowed_countries
-      @allowed_countries ||= SiteSetting.geoblocking_allowed_countries.upcase.split("|")
+      cache['geoblocking_allowed_countries'] ||= SiteSetting.geoblocking_allowed_countries.upcase.split("|").to_set
     end
 
     def self.allowed_geoname_ids
-      @allowed_geoname_ids ||= SiteSetting.geoblocking_allowed_geoname_ids.split("|").map(&:to_i).to_set
+      cache['geoblocking_allowed_geoname_ids'] ||= SiteSetting.geoblocking_allowed_geoname_ids.split("|").map(&:to_i).to_set
     end
 
     def self.blocked_countries
-      @blocked_countries ||= SiteSetting.geoblocking_blocked_countries.upcase.split("|")
+      cache['geoblocking_blocked_countries'] ||= SiteSetting.geoblocking_blocked_countries.upcase.split("|").to_set
     end
 
     def self.blocked_geoname_ids
-      @blocked_geoname_ids ||= SiteSetting.geoblocking_blocked_geoname_ids.split("|").map(&:to_i).to_set
-    end
-
-    def self.reset_settings_cache!
-      @allowed_countries = nil
-      @allowed_geoname_ids = nil
-      @blocked_countries = nil
-      @blocked_geoname_ids = nil
+      cache['geoblocking_blocked_geoname_ids'] ||= SiteSetting.geoblocking_blocked_geoname_ids.split("|").map(&:to_i).to_set
     end
   end
 
   on(:site_setting_changed) do |name, old_value, new_value|
-    if [:geoblocking_enabled,
-        :geoblocking_allowed_countries,
+    if [:geoblocking_allowed_countries,
         :geoblocking_allowed_geoname_ids,
         :geoblocking_blocked_countries,
         :geoblocking_blocked_geoname_ids].include?(name)
-      DiscourseGeoblocking.reset_settings_cache!
+      DiscourseGeoblocking.reset_cache!
     end
   end
 end
