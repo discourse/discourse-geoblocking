adf7ac44e3ac06f198cd5c81a33311ae4187d72a
David Taylor <david@taylorhq.com>
2023-01-23 17:47:19 +0000

    DEV: Add syntax_tree config and apply formatting (#33)

--- spec/requests/geoblocking_spec.rb
+++ spec/requests/geoblocking_spec.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe GeoblockingController do
-  before do
-    SiteSetting.geoblocking_enabled = true
-  end
+  before { SiteSetting.geoblocking_enabled = true }
 
-  it 'does not redirect users in a loop if login_required' do
+  it "does not redirect users in a loop if login_required" do
     SiteSetting.login_required = true
-    SiteSetting.geoblocking_allowed_countries = 'EU'
+    SiteSetting.geoblocking_allowed_countries = "EU"
 
     get Discourse.base_url
     expect(response.status).to eq(403)
-    expect(response.body).to include('Access forbidden based on location.')
+    expect(response.body).to include("Access forbidden based on location.")
   end
 
-  context "RSS feed and login_required" do
+  describe "RSS feed and login_required" do
     before do
       SiteSetting.login_required = true
-      DiscourseIpInfo.open_db(File.join(Rails.root, 'spec', 'fixtures', 'mmdb'))
+      DiscourseIpInfo.open_db(File.join(Rails.root, "spec", "fixtures", "mmdb"))
     end
 
     let(:us_ip) { "216.160.83.56" }
@@ -28,24 +26,24 @@ describe GeoblockingController do
     let(:rss_path) { "/latest.rss?api_key=#{api_key.key}&api_username=#{user.username_lower}" }
     let(:json_path) { "/latest.json?api_key=#{api_key.key}&api_username=#{user.username_lower}" }
 
-    it 'allows access if not blocked country' do
-      SiteSetting.geoblocking_blocked_countries = 'EU'
+    it "allows access if not blocked country" do
+      SiteSetting.geoblocking_blocked_countries = "EU"
 
-      get rss_path, env: { "REMOTE_ADDR": us_ip }
+      get rss_path, env: { REMOTE_ADDR: us_ip }
       expect(response.status).to eq(200)
 
       # Confirm it's still not allowed for json
-      get json_path, env: { "REMOTE_ADDR": us_ip }
+      get json_path, env: { REMOTE_ADDR: us_ip }
       expect(response.status).to eq(403)
     end
 
-    it 'prevents access if blocked country' do
-      SiteSetting.geoblocking_blocked_countries = 'US'
+    it "prevents access if blocked country" do
+      SiteSetting.geoblocking_blocked_countries = "US"
 
-      get rss_path, env: { "REMOTE_ADDR": us_ip }
+      get rss_path, env: { REMOTE_ADDR: us_ip }
       expect(response.status).to eq(403)
 
-      get json_path, env: { "REMOTE_ADDR": us_ip }
+      get json_path, env: { REMOTE_ADDR: us_ip }
       expect(response.status).to eq(403)
     end
   end
